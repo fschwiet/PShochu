640b66b1d7dbeb2a71b4fcb5113bca76e54b1510
fschwiet <fschwiet@gmail.com>
2011-03-19 13:21:22 -0700

    added some initial tests

---
+++ PShochu.Tests/can_run_remote_interactive_tasks.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NJasmine;
+
+namespace PShochu.Tests
+{
+    public class can_run_remote_interactive_tasks : GivenWhenThenFixture
+    {
+        public override void Specify()
+        {
+            given("a psake script which writes the current process id to output", delegate
+            {
+                when("that script is invoked interactively", delegate
+                {
+                    then("he output file has a different process id than the current", delegate
+                    {
+                        
+                    });
+                });
+            });
+
+            given("a vhd image (64bit?  32bit?)", delegate
+            {
+                given("a psake script that writes its IP address and process id", delegate
+                {
+                    when("the script is invoked remotely", delegate
+                    {
+                        then("the reported ip address and process id are different", delegate
+                        {
+                            
+                        });
+                    });
+                });
+            });
+        }
+    }
+}

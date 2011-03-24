0225994e5138d157d257918cc53957514e2200ce
fschwiet <fschwiet@gmail.com>
2011-03-23 23:34:09 -0700

    adding some pinvoke declarations

---
+++ PShochu/AdvApi32PInvoke.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace PShochu
+{
+    public class AdvApi32PInvoke
+    {
+        [DllImport("advapi32.dll", SetLastError = true)]
+        public static extern bool LogonUser(
+            string lpszUsername,
+            string lpszDomain,
+            string lpszPassword,
+            int dwLogonType,
+            int dwLogonProvider,
+            out IntPtr phToken);
+
+        public enum LogonType
+        {
+            /// <summary>
+            /// This logon type is intended for users who will be interactively using the computer, such as a user being logged on  
+            /// by a terminal server, remote shell, or similar process.
+            /// This logon type has the additional expense of caching logon information for disconnected operations;
+            /// therefore, it is inappropriate for some client/server applications,
+            /// such as a mail server.
+            /// </summary>
+            LOGON32_LOGON_INTERACTIVE = 2,
+
+            /// <summary>
+            /// This logon type is intended for high performance servers to authenticate plaintext passwords.
+
+            /// The LogonUser function does not cache credentials for this logon type.
+            /// </summary>
+            LOGON32_LOGON_NETWORK = 3,
+
+            /// <summary>
+            /// This logon type is intended for batch servers, where processes may be executing on behalf of a user without
+            /// their direct intervention. This type is also for higher performance servers that process many plaintext
+            /// authentication attempts at a time, such as mail or Web servers.
+            /// The LogonUser function does not cache credentials for this logon type.
+            /// </summary>
+            LOGON32_LOGON_BATCH = 4,
+
+            /// <summary>
+            /// Indicates a service-type logon. The account provided must have the service privilege enabled.
+            /// </summary>
+            LOGON32_LOGON_SERVICE = 5,
+
+            /// <summary>
+            /// This logon type is for GINA DLLs that log on users who will be interactively using the computer.
+            /// This logon type can generate a unique audit record that shows when the workstation was unlocked.
+            /// </summary>
+            LOGON32_LOGON_UNLOCK = 7,
+
+            /// <summary>
+            /// This logon type preserves the name and password in the authentication package, which allows the server to make
+            /// connections to other network servers while impersonating the client. A server can accept plaintext credentials
+            /// from a client, call LogonUser, verify that the user can access the system across the network, and still
+            /// communicate with other servers.
+            /// NOTE: Windows NT:  This value is not supported.
+            /// </summary>
+            LOGON32_LOGON_NETWORK_CLEARTEXT = 8,
+
+            /// <summary>
+            /// This logon type allows the caller to clone its current token and specify new credentials for outbound connections.
+            /// The new logon session has the same local identifier but uses different credentials for other network connections.
+            /// NOTE: This logon type is supported only by the LOGON32_PROVIDER_WINNT50 logon provider.
+            /// NOTE: Windows NT:  This value is not supported.
+            /// </summary>
+            LOGON32_LOGON_NEW_CREDENTIALS = 9,
+        }
+
+        public enum LogonProvider
+        {
+            /// <summary>
+            /// Use the standard logon provider for the system.
+            /// The default security provider is negotiate, unless you pass NULL for the domain name and the user name
+            /// is not in UPN format. In this case, the default provider is NTLM.
+            /// NOTE: Windows 2000/NT:   The default security provider is NTLM.
+            /// </summary>
+            LOGON32_PROVIDER_DEFAULT = 0,
+        }
+
+        [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern bool CreateProcessWithLogonW(
+           String userName,
+           String domain,
+           String password,
+           LogonFlags logonFlags,
+           String applicationName,
+           String commandLine,
+           CreationFlags creationFlags,
+           UInt32 environment,
+           String currentDirectory,
+           ref STARTUPINFO startupInfo,
+           out PROCESS_INFORMATION processInformation);
+
+        [Flags]
+        public enum CreationFlags
+        {
+            CREATE_SUSPENDED = 0x00000004,
+            CREATE_NEW_CONSOLE = 0x00000010,
+            CREATE_NEW_PROCESS_GROUP = 0x00000200,
+            CREATE_UNICODE_ENVIRONMENT = 0x00000400,
+            CREATE_SEPARATE_WOW_VDM = 0x00000800,
+            CREATE_DEFAULT_ERROR_MODE = 0x04000000,
+        }
+
+        [Flags]
+        public enum LogonFlags
+        {
+            LOGON_WITH_PROFILE = 0x00000001,
+            LOGON_NETCREDENTIALS_ONLY = 0x00000002
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        public struct STARTUPINFO
+        {
+            public Int32 cb;
+            public string lpReserved;
+            public string lpDesktop;
+            public string lpTitle;
+            public Int32 dwX;
+            public Int32 dwY;
+            public Int32 dwXSize;
+            public Int32 dwYSize;
+            public Int32 dwXCountChars;
+            public Int32 dwYCountChars;
+            public Int32 dwFillAttribute;
+            public Int32 dwFlags;
+            public Int16 wShowWindow;
+            public Int16 cbReserved2;
+            public IntPtr lpReserved2;
+            public IntPtr hStdInput;
+            public IntPtr hStdOutput;
+            public IntPtr hStdError;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct PROCESS_INFORMATION
+        {
+            public IntPtr hProcess;
+            public IntPtr hThread;
+            public int dwProcessId;
+            public int dwThreadId;
+        }
+    }
+}

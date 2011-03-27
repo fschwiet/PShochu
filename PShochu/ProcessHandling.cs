c6a129b6f3528d2104194db96b22ea3cf183673a
fschwiet <fschwiet@gmail.com>
2011-03-26 18:42:15 -0700

    reordered some functions

--- PShochu/ProcessHandling.cs
+++ PShochu/ProcessHandling.cs
@@ -14,6 +14,52 @@ namespace PShochu
 {
     public class ProcessHandling
     {
+        public static InvokeResult InvokeScript(string moduleLocation, string scriptPath, string taskName = "default")
+        {
+            InvokeResult result = null;
+            int? exitCode = null;
+
+            var consoleStream = new MemoryStream();
+            var errorStream = new MemoryStream();
+
+            try
+            {
+                using(var consoleWriter = new NonclosingStreamWriter(consoleStream))
+                using(var errorWriter = new NonclosingStreamWriter(errorStream))
+                {
+                    exitCode = InvokeScript(moduleLocation, scriptPath, taskName, consoleWriter.WriteLine, errorWriter.WriteLine);
+
+                    consoleWriter.Flush();
+                    errorWriter.Flush();
+
+                    consoleStream.Seek(0, SeekOrigin.Begin);
+                    errorStream.Seek(0, SeekOrigin.Begin);
+
+                    var consoleOutput =
+                        new NonclosingStreamReader(consoleStream).ReadToEnd().Split(new[] { consoleWriter.NewLine },
+                            StringSplitOptions.None);
+
+                    var errorOutput =
+                        new NonclosingStreamReader(errorStream).ReadToEnd().Split(new[] { consoleWriter.NewLine },
+                            StringSplitOptions.None);
+
+                    result = new InvokeResult(consoleStream, errorStream, consoleOutput, errorOutput, exitCode);
+                    consoleStream = null;
+                    errorStream = null;
+                }
+            }
+            finally
+            {
+                if (consoleStream != null)
+                    consoleStream.Dispose();
+                
+                if (errorStream != null)
+                    errorStream.Dispose();
+            }
+
+            return result;
+        }
+
         public static int InvokeScript(string moduleLocation, string scriptPath, string taskName, Action<string> onConsoleOut, Action<string> onErrorOut)
         {
             ProcessStartInfo psi = new ProcessStartInfo();
@@ -65,52 +111,6 @@ namespace PShochu
             arguments.Append(String.Format(@"invoke-psake ""{0}"" {1};", psakeScriptPath, taskName));
 
             return "-NoProfile -Noninteractive -EncodedCommand " + Base64Encode(arguments.ToString());
-        }
-
-        public static InvokeResult InvokeScript(string moduleLocation, string scriptPath, string taskName = "default")
-        {
-            InvokeResult result = null;
-            int? exitCode = null;
-
-            var consoleStream = new MemoryStream();
-            var errorStream = new MemoryStream();
-
-            try
-            {
-                using(var consoleWriter = new NonclosingStreamWriter(consoleStream))
-                using(var errorWriter = new NonclosingStreamWriter(errorStream))
-                {
-                    exitCode = InvokeScript(moduleLocation, scriptPath, taskName, consoleWriter.WriteLine, errorWriter.WriteLine);
-
-                    consoleWriter.Flush();
-                    errorWriter.Flush();
-
-                    consoleStream.Seek(0, SeekOrigin.Begin);
-                    errorStream.Seek(0, SeekOrigin.Begin);
-
-                    var consoleOutput =
-                        new NonclosingStreamReader(consoleStream).ReadToEnd().Split(new[] { consoleWriter.NewLine },
-                            StringSplitOptions.None);
-
-                    var errorOutput =
-                        new NonclosingStreamReader(errorStream).ReadToEnd().Split(new[] { consoleWriter.NewLine },
-                            StringSplitOptions.None);
-
-                    result = new InvokeResult(consoleStream, errorStream, consoleOutput, errorOutput, exitCode);
-                    consoleStream = null;
-                    errorStream = null;
-                }
-            }
-            finally
-            {
-                if (consoleStream != null)
-                    consoleStream.Dispose();
-                
-                if (errorStream != null)
-                    errorStream.Dispose();
-            }
-
-            return result;
         }
 
         public static string Base64Encode(string input)

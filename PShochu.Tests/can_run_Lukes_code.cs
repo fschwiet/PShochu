91744b308a22d0afc2c109eec8f012818e14dd24
fschwiet <fschwiet@gmail.com>
2011-03-30 20:44:28 -0700

    step1

--- PShochu.Tests/can_run_Lukes_code.cs
+++ PShochu.Tests/can_run_Lukes_code.cs
@@ -6,6 +6,7 @@ using System.Runtime.InteropServices;
 using System.Text;
 using NJasmine;
 using PShochu.PInvoke;
+using PShochu.PInvoke.NetWrappers;
 
 namespace PShochu.Tests
 {
@@ -13,38 +14,76 @@ namespace PShochu.Tests
     {
         public override void Specify()
         {
+            it("can run code like Luke's code", delegate
+            {
+                IntPtr hToken = AccessToken.GetCurrentAccessToken().DangerousGetHandle();
+
+                //if(!AdvApi32PInvoke.OpenProcessToken(Kernel32.GetCurrentProcess(), AdvApi32PInvoke.TOKEN_DUPLICATE, out hToken))
+                //{
+                //    throw new Win32Exception(Marshal.GetLastWin32Error());
+                //}
+
+                IntPtr hDuplicate;
+                if (AdvApi32PInvoke.DuplicateTokenEx(hToken,
+                    AdvApi32PInvoke.TOKEN_ASSIGN_PRIMARY |
+                    AdvApi32PInvoke.TOKEN_DUPLICATE |
+                    AdvApi32PInvoke.TOKEN_QUERY |
+                    AdvApi32PInvoke.TOKEN_ADJUST_DEFAULT |
+                    AdvApi32PInvoke.TOKEN_ADJUST_SESSIONID,
+                    Constants.NULL, AdvApi32PInvoke.SECURITY_IMPERSONATION_LEVEL.SecurityImpersonation,
+                    AdvApi32PInvoke.TOKEN_TYPE.TokenPrimary, out hDuplicate))
+                {
+                    string commandLine = ("powershell");
+
+                    AdvApi32PInvoke.STARTUPINFO startupInfo = new AdvApi32PInvoke.STARTUPINFO();
+                    startupInfo.cb = Marshal.SizeOf(typeof(AdvApi32PInvoke.STARTUPINFO));
+
+                    AdvApi32PInvoke.PROCESS_INFORMATION processInformation =
+                        new AdvApi32PInvoke.PROCESS_INFORMATION();
+                    if (
+                        !AdvApi32PInvoke.CreateProcessWithTokenW(hDuplicate,
+                            AdvApi32PInvoke.LogonFlags.LOGON_WITH_PROFILE, null, commandLine, 0, Constants.NULL,
+                            Constants.NULL, ref startupInfo, out processInformation))
+                    {
+                        throw new Win32Exception(Marshal.GetLastWin32Error());
+                    }
+                }
+                else
+                {
+                    throw new Win32Exception(Marshal.GetLastWin32Error());
+                }
+            }); 
+            
             it("can run Luke's code", delegate
             {
                 IntPtr hToken;
 
-                if(AdvApi32PInvoke.OpenProcessToken(Kernel32.GetCurrentProcess(), AdvApi32PInvoke.TOKEN_DUPLICATE, out hToken))
+                if (!AdvApi32PInvoke.OpenProcessToken(Kernel32.GetCurrentProcess(), AdvApi32PInvoke.TOKEN_DUPLICATE, out hToken))
                 {
-                    IntPtr hDuplicate;
-                    if (AdvApi32PInvoke.DuplicateTokenEx(hToken,
-                        AdvApi32PInvoke.TOKEN_ASSIGN_PRIMARY |
-                        AdvApi32PInvoke.TOKEN_DUPLICATE |
-                        AdvApi32PInvoke.TOKEN_QUERY |
-                        AdvApi32PInvoke.TOKEN_ADJUST_DEFAULT |
-                        AdvApi32PInvoke.TOKEN_ADJUST_SESSIONID,
-                        Constants.NULL, AdvApi32PInvoke.SECURITY_IMPERSONATION_LEVEL.SecurityImpersonation,
-                        AdvApi32PInvoke.TOKEN_TYPE.TokenPrimary, out hDuplicate))
-                    {
-                        string commandLine = ("C:\\Windows\\system32\\notepad.exe");
+                    throw new Win32Exception(Marshal.GetLastWin32Error());
+                }
 
-                        AdvApi32PInvoke.STARTUPINFO startupInfo = new AdvApi32PInvoke.STARTUPINFO();
-                        startupInfo.cb = Marshal.SizeOf(typeof (AdvApi32PInvoke.STARTUPINFO));
+                IntPtr hDuplicate;
+                if (AdvApi32PInvoke.DuplicateTokenEx(hToken,
+                    AdvApi32PInvoke.TOKEN_ASSIGN_PRIMARY |
+                    AdvApi32PInvoke.TOKEN_DUPLICATE |
+                    AdvApi32PInvoke.TOKEN_QUERY |
+                    AdvApi32PInvoke.TOKEN_ADJUST_DEFAULT |
+                    AdvApi32PInvoke.TOKEN_ADJUST_SESSIONID,
+                    Constants.NULL, AdvApi32PInvoke.SECURITY_IMPERSONATION_LEVEL.SecurityImpersonation,
+                    AdvApi32PInvoke.TOKEN_TYPE.TokenPrimary, out hDuplicate))
+                {
+                    string commandLine = ("powershell");
 
-                        AdvApi32PInvoke.PROCESS_INFORMATION processInformation =
-                            new AdvApi32PInvoke.PROCESS_INFORMATION();
-                        if (
-                            !AdvApi32PInvoke.CreateProcessWithTokenW(hDuplicate,
-                                AdvApi32PInvoke.LogonFlags.LOGON_WITH_PROFILE, null, commandLine, 0, Constants.NULL,
-                                Constants.NULL, ref startupInfo, out processInformation))
-                        {
-                            throw new Win32Exception(Marshal.GetLastWin32Error());
-                        }
-                    }
-                    else
+                    AdvApi32PInvoke.STARTUPINFO startupInfo = new AdvApi32PInvoke.STARTUPINFO();
+                    startupInfo.cb = Marshal.SizeOf(typeof(AdvApi32PInvoke.STARTUPINFO));
+
+                    AdvApi32PInvoke.PROCESS_INFORMATION processInformation =
+                        new AdvApi32PInvoke.PROCESS_INFORMATION();
+                    if (
+                        !AdvApi32PInvoke.CreateProcessWithTokenW(hDuplicate,
+                            AdvApi32PInvoke.LogonFlags.LOGON_WITH_PROFILE, null, commandLine, 0, Constants.NULL,
+                            Constants.NULL, ref startupInfo, out processInformation))
                     {
                         throw new Win32Exception(Marshal.GetLastWin32Error());
                     }
